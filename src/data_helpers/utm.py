2f1a7a0c3913dd743cfc2f252f0638ce0cfd6c5a
Nick Bolten <nbolten@gmail.com>
2018-04-25 19:45:11 -0700

    Added mid-block crosswalks

--- src/data_helpers/utm.py
+++ src/data_helpers/utm.py
@@ -2,3 +2,14 @@ def lonlat_to_utm_epsg(lon, lat):
     utm_zone_epsg = 32700 - 100 * round((45 + lat) / 90.) + \
         round((183 + lon) / 6.)
     return utm_zone_epsg
+
+
+def gdf_wgs84_to_utm(gdf):
+    # Random point - should probably find centroid or something
+    lon, lat = gdf.iloc[0]['geometry'].coords[0]
+    zone = lonlat_to_utm_epsg(lon, lat)
+
+    gdf.crs = {'init': 'epsg:4326'}
+    new = gdf.to_crs({'init': 'epsg:{}'.format(zone)})
+
+    return new
