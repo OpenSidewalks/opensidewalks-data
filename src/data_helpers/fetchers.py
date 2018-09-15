54ba860f1cf171b43c48d532f4b584163c9220ea
= <=>
2018-09-14 17:37:35 -0700

    Added Bellingham workflow, updated data_helpers module to have more general-purpose zip file fetching

--- src/data_helpers/fetchers.py
+++ src/data_helpers/fetchers.py
@@ -8,21 +8,16 @@ import geopandas as gpd
 import requests
 
 
-def fetch_shapefile(url, shp_path, bounds=None):
+def fetch_shapefile(url, unzipped_path, bounds=None):
     # Download based on source.json layer url
-    response = requests.get(url)
     tempdir = tempfile.mkdtemp()
-    try:
-        archive = ZipFile(BytesIO(response.content))
-        archive.extractall(tempdir)
-        shp_path = os.path.join(tempdir, '{}.shp'.format(shp_path))
+    shp_filename = os.path.basename(unzipped_path)
+    shp_path = os.path.join(tempdir, shp_filename)
 
-        if not os.path.isfile(shp_path):
-            raise Exception('Could not find matching files in zip archive.')
+    fetch_and_unzip(url, unzipped_path, shp_path)
 
-        gdf = gpd.read_file(os.path.join(tempdir, shp_path))
-    finally:
-        shutil.rmtree(tempdir)
+    gdf = gpd.read_file(shp_path)
+
     # Remove invalid geometries, as they will create invalid GeoJSON and break
     # fiona
     gdf = gdf[~gdf.geometry.isnull()]
@@ -34,3 +29,36 @@ def fetch_shapefile(url, shp_path, bounds=None):
         gdf = gdf.loc[[q.object for q in query]]
 
     return gdf
+
+
+def fetch_gdb(url, expanded_path, destination, bounds=None):
+    fetch_and_unzip(url, expanded_path, destination)
+    gdf = gpd.read_file(destination)
+
+    # Remove invalid geometries, as they will create invalid GeoJSON and break
+    # fiona
+    gdf = gdf[~gdf.geometry.isnull()]
+    gdf = gdf[~gdf.geometry.is_empty]
+    # Convert to wgs84 and require that values be within the bounding box
+    gdf = gdf.to_crs({'init': 'epsg:4326'})
+    if bounds is not None:
+        query = gdf.sindex.intersection(bounds, objects=True)
+        gdf = gdf.loc[[q.object for q in query]]
+
+    return gdf
+
+
+def fetch_and_unzip(url, expanded_path, destination):
+    response = requests.get(url)
+    tempdir = tempfile.mkdtemp()
+    try:
+        archive = ZipFile(BytesIO(response.content))
+        archive.extractall(tempdir)
+        unzipped_path = os.path.join(tempdir, expanded_path)
+
+        if not os.path.exists(unzipped_path):
+            raise Exception('Could not find matching files in zip archive.')
+
+        os.rename(unzipped_path, destination)
+    finally:
+        shutil.rmtree(tempdir)
