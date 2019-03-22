1b642f9c2cb9db4f4f0f5b503e6a281a72b14e9d
= <=>
2019-03-22 13:56:16 -0700

    Better centerpoint for Bellingham

--- merge.py
+++ merge.py
@@ -6,15 +6,18 @@ from shapely.geometry import MultiPoint, mapping
 
 CITY_DATA = [{
     "name": "Seattle",
+    "key": "wa.seattle",
     "center": [-122.339, 47.604, 15],
     "transportation_data": "./cities/seattle/output/transportation.geojson",
 }, {
     "name": "Mt. Vernon",
+    "key": "wa.mtvernon",
     "center": [-122.336, 48.419, 14],
     "transportation_data": "./cities/mtvernon/output/transportation.geojson",
 }, {
     "name": "Bellingham",
-    "center": [-122.47871,48.75253,13.5],
+    "key": "wa.bellingham",
+    "center": [-122.478, 48.751, 13.5],
     "transportation_data": "./cities/bellingham/output/transportation.geojson"
 }]
 
@@ -33,10 +36,15 @@ def merge_geojson(datasets):
 
         points = MultiPoint(points)
         hull = points.convex_hull
+
+        bounds = list(hull.bounds);
+
         datasets_metadata["features"].append({
             "type": "Feature",
             "geometry": mapping(hull),
             "properties": {
+                "bounds": bounds,
+                "key": dataset["key"],
                 "name": dataset["name"],
                 "lon": dataset["center"][0],
                 "lat": dataset["center"][1],
@@ -53,5 +61,5 @@ if __name__ == "__main__":
         os.mkdir("./merged")
     with open("./merged/transportation.geojson", "w") as f:
         json.dump(merged, f)
-    with open("./merged/areas_served.geojson", "w") as f:
+    with open("./merged/regions.geojson", "w") as f:
         json.dump(datasets_metadata, f)
